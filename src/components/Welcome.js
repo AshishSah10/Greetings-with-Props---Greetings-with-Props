b04ecc8fc5bf67a8fea67b51b4eb6be911119041
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2021-03-11 22:07:08 +0530

    create Welcome.js create Welcome.js modify App.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,11 @@
 // write code for Welcome component here
 import React from "react";
 
-function Welcome(props){
+export default function Welcome({ name }) {
   return (
-      <>
-        <h1>Hey ! {props.name}</h1>
-        <h2>Welcome to Newton School.</h2>
-     </>
-  )
+    <>
+      <h1>Hey !{name}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
 }
-
-
-export default Welcome;
